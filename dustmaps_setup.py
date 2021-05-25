95f7df2e9d779847d469115887e46da689c5c1f5
Cameron <cmeron9@gmail.com>
2021-05-25 17:08:10 +0100

    4 plotter spatial unbroken, still needs work

--- dustmaps_setup.py
+++ dustmaps_setup.py
@@ -1,6 +1,6 @@
-from dustmaps.config import config
-#insert your preferred file path for map storage here
-config['data_dir'] = 'file/path/to/put/maps/in'
-
 import dustmaps.sfd
+from dustmaps.config import config
+# insert your preferred file path for map storage here
+config['data_dir'] = '/home/robertsoncl/ifa/'
+
 dustmaps.sfd.fetch()
--- subhess.py
+++ subhess.py
@@ -5,54 +5,55 @@ from scipy import *
 from matplotlib import *
 rcdefaults()
 
-def create_hess_diagram(xdata,ydata,ax,label,xlims=[-1,4.5], ylims=[21,11],levels='None', colormap=cm.plasma,cbarr='None', cbarrtitle='', xlab='$J_0$-$K_0$', ylab='$K_0$'):
-    rc('axes',labelsize=25)
-    ### Plot all stars in CMD as small points
-    ax.plot(xdata,ydata,linestyle='none',marker ='.',ms=1,color='black',zorder=1,label=label)
 
+def create_hess_diagram(xdata, ydata, ax, label, xlims=[-1, 4.5], ylims=[21, 11], levels='None', colormap=cm.plasma, cbarr='None', cbarrtitle='', xlab='$J_0$-$K_0$', ylab='$K_0$'):
+    rc('axes', labelsize=25)
+    # Plot all stars in CMD as small points
+    ax.plot(xdata, ydata, linestyle='none', marker='.',
+            ms=1, color='black', zorder=1, label=label)
 
+    # Use color and magnitude data to split and average into a binned 2D histogram
+    Z, xedges, yedges = np.histogram2d(xdata, ydata, bins=(300, 300))
 
-    ### Use color and magnitude data to split and average into a binned 2D histogram
-    Z, xedges, yedges = np.histogram2d(xdata,ydata,bins=(300,300))
-    
-    ### Find the cell centers from the cell edges
+    # Find the cell centers from the cell edges
     x = 0.5*(xedges[:-1] + xedges[1:])
     y = 0.5*(yedges[:-1] + yedges[1:])
-    
-    ### Promote to 2D arrays
+
+    # Promote to 2D arrays
     Y, X = np.meshgrid(y, x)
-    
-    ### Mask out values without data
-    Z = np.ma.array(Z, mask=(Z==0))
-    
-    ### Set levels options as default or user-defined option
-    if levels=='None':
-        cntr=ax.contourf(X,Y,Z,cmap=colormap,zorder=2)
+
+    # Mask out values without data
+    Z = np.ma.array(Z, mask=(Z == 0))
+
+    # Set levels options as default or user-defined option
+    if levels == 'None':
+        cntr = ax.contourf(X, Y, Z, cmap=colormap, zorder=2)
     else:
-        cntr=ax.contourf(X,Y,Z,levels=levels,cmap=colormap,zorder=2)
+        cntr = ax.contourf(X, Y, Z, levels=levels, cmap=colormap, zorder=2)
     cntr.cmap.set_under('white')
-    
-    ### Set plot limits as default or user-defined option
-    #xlim(xlims[0],xlims[1]);ylim(ylims[0],ylims[1])
-    
-    ### Determine whether to plot a color bar
-    if cbarr!='None':
+
+    # Set plot limits as default or user-defined option
+    # xlim(xlims[0],xlims[1]);ylim(ylims[0],ylims[1])
+
+    # Determine whether to plot a color bar
+    if cbarr != 'None':
         cbar_ax = fig.add_axes([0.91, 0.14, 0.02, 0.7])
-        d=fig.colorbar(cntr, cax=cbar_ax)#, title=cbarrtitle)
-        d.set_label(label=cbarrtitle,size=10)
+        d = fig.colorbar(cntr, cax=cbar_ax)  # , title=cbarrtitle)
+        d.set_label(label=cbarrtitle, size=10)
 
 #matplotlib.rc('font',family='Bitstream Vera Serif')
 
+
 class subhess:
 
-    def plot_kj_cmd(galaxy_object,ax,label):
+    def plot_kj_cmd(galaxy_object, ax, label):
         """
         plotHess(mag,color) is a function intended to create a contour plot of stellar density in the canonical Color-Magnitude Diagram (CMD). plotHess() plots all the stars in the CMD and then overplots a contour of their density.
         mag = magnitudes of the stars
         color = colors of the stars
         mag and color are required to run plotHess() and need to be the same length arrays.
         Optional keyword arguments:
-        
+
         =========   =======================================================
         Keyword     Description
         =========   =======================================================
@@ -65,25 +66,23 @@ class subhess:
         xlabel:     sets the xlabel for the plot; should be string
         ylabel:     sets the ylabel for the plot; should be string
         saveas:     pathway for saving the output plot. The default is to save in the same folder as "HessCMD.png"
-        
-        
+
+
         """
-        ### Set figure options
-        
-        galaxy_data=galaxy_object.data
-        
-        xdata=galaxy_data.jmag-galaxy_data.kmag
-        ydata=galaxy_data.kmag
-        
-        create_hess_diagram(xdata,ydata,ax,label)
-        
-    
-    def plot_cc(galaxy_object,ax,label):
-        
-        galaxy_data=galaxy_object.data
-        
-        xdata=galaxy_data.jmag-galaxy_data.hmag
-        ydata=galaxy_data.hmag-galaxy_data.kmag
-        
-        create_hess_diagram(xdata,ydata,ax,label)
-        
+        # Set figure options
+
+        galaxy_data = galaxy_object.data
+
+        xdata = galaxy_data.jmag-galaxy_data.kmag
+        ydata = galaxy_data.kmag
+
+        create_hess_diagram(xdata, ydata, ax, label)
+
+    def plot_cc(galaxy_object, ax, label):
+
+        galaxy_data = galaxy_object.data
+
+        xdata = galaxy_data.jmag-galaxy_data.hmag
+        ydata = galaxy_data.hmag-galaxy_data.kmag
+
+        create_hess_diagram(xdata, ydata, ax, label)
